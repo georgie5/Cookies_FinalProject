4c6b0fc4391e81ba636332cd9e840be6d49e35f4
Aldric2023 <2020151626@ub.edu.bz>
2023-05-08 22:44:32 -0600

    Confidential (encrypted) and tamper-proof cookies

--- 4confidential_encrypted/main.go
+++ 4confidential_encrypted/main.go
@@ -1,9 +1,76 @@
 package main
 
-import(
-	"fmt"
+import (
+	"encoding/hex"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/georgie5/Aldric2023/CookiesFinalProject/confidential/internal/cookies"
 )
 
-func main(){
-	fmt.Println("Confidential (encrypted) and tamper-proof cookies")
+// Declare a global variable to hold the secret key.
+var secretKey []byte
+
+func main() {
+	var err error
+
+	// Decode the random 64-character hex string to give us a slice containing
+	// 32 random bytes. For simplicity, I've hardcoded this hex string but in a
+	// real application you should read it in at runtime from a command-line
+	// flag or environment variable.
+	secretKey, err = hex.DecodeString("13d6b4dff8f84a10851021ec8608f814570d562c92fe6b5ec4c9f595bcb3234b")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set", setCookieHandler)
+	mux.HandleFunc("/get", getCookieHandler)
+
+	log.Print("Listening...")
+	err = http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func setCookieHandler(w http.ResponseWriter, r *http.Request) {
+    cookie := http.Cookie{
+        Name:     "exampleCookie",
+        Value:    "Confidential information is located here",
+        Path:     "/",
+        MaxAge:   3600,
+        HttpOnly: true,
+        Secure:   true,
+        SameSite: http.SameSiteLaxMode,
+    }
+
+    err := cookies.WriteEncrypted(w, cookie, secretKey)
+    if err != nil {
+        log.Println(err)
+        http.Error(w, "server error", http.StatusInternalServerError)
+        return
+    }
+
+    w.Write([]byte("cookie set!"))
+}
+
+func getCookieHandler(w http.ResponseWriter, r *http.Request) {
+    value, err := cookies.ReadEncrypted(r, "exampleCookie", secretKey)
+    if err != nil {
+        switch {
+        case errors.Is(err, http.ErrNoCookie):
+            http.Error(w, "cookie not found", http.StatusBadRequest)
+        case errors.Is(err, cookies.ErrInvalidValue):
+            http.Error(w, "invalid cookie", http.StatusBadRequest)
+        default:
+            log.Println(err)
+            http.Error(w, "server error", http.StatusInternalServerError)
+        }
+        return
+    }
+
+    w.Write([]byte(value))
 }
